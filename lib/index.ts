be87f242e4688c1783e1d1ec9b02e13a8769bab1
Thomas Meadows <thomas@itpro.tv>
2017-05-14 09:40:28 -0400

    modified server to no longer need to use policies

--- lib/index.ts
+++ lib/index.ts
@@ -1,77 +1,64 @@
 export function DHapi(): any {
-  return function(OriginalClassConstructor: any) {
-
-    return function () {
-      const origin = new OriginalClassConstructor();
-      origin.__seatbelt__ = 'server';
-      origin.__seatbelt_strap__ = function(classesByType: any) {
-        this.hapi = require('hapi');
-        this.app = new this.hapi.Server();
-        this.port = process.env.port || 3000;
-        this.__controller_wrapper__ = function (controllerFunctions: Function[], req: any, reply: Function) {
-          const nextWrapper = (i: number): any => {
-            if (!controllerFunctions[i]) {
-              return reply({status: 'request failed'}).code(500);
-            }
-            return controllerFunctions[i]({
-              send: (...params: any[]) => reply(...params),
-              next: () => nextWrapper(1 + i),
-              params: Object.assign(
-                {},
-                typeof req.params === 'object' ? req.params : {},
-                typeof req.body === 'object' ? req.body : {}
-                ,
-                typeof req.payload === 'object' ? req.payload : {}
-                ,
-                typeof req.query === 'object' ? req.query : {}
-              )
-            }, {
-              req,
-              reply
-            });
-          };
-          nextWrapper(0);
-        };
-
-        this.app.connection({ port: this.port });
-
-        if (classesByType['route']) {
-          classesByType['route'].forEach((route: any) => {
-
-            const policies: any[] = [];
-            route.__seatbelt_config__.policies.forEach((routePolicyName: string) => {
-              classesByType['policy'].forEach((policy: any) => {
-                if (routePolicyName === policy.__name__) {
-                  policies.push(policy.controller);
-                }
+  return function(OriginalClassConstructor: new() => any) {
+    return class extends OriginalClassConstructor {
+      public __seatbelt__: string;
+      public __seatbelt_strap__: Function;
+      constructor() {
+        super();
+        this.__seatbelt__ = 'server';
+        this.__seatbelt_strap__ = function(classesByType: any) {
+          this.hapi = require('hapi');
+          this.app = new this.hapi.Server();
+          this.port = process.env.port || 3000;
+          this.__controller_wrapper__ = function (controllerFunction: Function, req: any, reply: Function) {
+            const nextWrapper = (i: number): any => {
+              if (!controllerFunction) {
+                return reply({status: 'request failed'}).code(500);
+              }
+              return controllerFunction({
+                send: (...params: any[]) => reply(...params),
+                params: Object.assign(
+                  {},
+                  typeof req.params === 'object' ? req.params : {},
+                  typeof req.body === 'object' ? req.body : {}
+                  ,
+                  typeof req.payload === 'object' ? req.payload : {}
+                  ,
+                  typeof req.query === 'object' ? req.query : {}
+                )
+              }, {
+                req,
+                reply
               });
-            });
+            };
+            nextWrapper(0);
+          };
 
-            const policiesPlusController = [
-              ...policies,
-              route.controller
-            ];
+          this.app.connection({ port: this.port });
 
-            route['__seatbelt_config__'].type.forEach((eachType: string) => {
-              route['__seatbelt_config__'].path.forEach((eachPath: string) => {
-                this.app.route({
+          if (classesByType['route']) {
+            classesByType['route'].forEach((route: any) => {
+
+              route['__seatbelt_config__'].type.forEach((eachType: string) => {
+                route['__seatbelt_config__'].path.forEach((eachPath: string) => {
+                  this.app.route({
                     method: eachType.toLowerCase(),
                     path: eachPath,
-                    handler: (request: any, reply: any) => origin.__controller_wrapper__(policiesPlusController, request, reply)
+                    handler: (request: any, reply: any) => this.__controller_wrapper__(route.controller, request, reply)
+                  });
                 });
               });
             });
-          });
-        }
-
-        this.app.start((err: Error) => {
-          if (err) {
-            throw err;
           }
-          console.log(`Server running at: ${this.app.info.uri}`);
-        });
+
+          this.app.start((err: Error) => {
+            if (err) {
+              throw err;
+            }
+            console.log(`Server running at: ${this.app.info.uri}`);
+          });
+        };
       };
-      return origin;
     };
   };
 }
